837c0f852a09437dfb8af09158de546d9953201b
arpita <arpita@stockholmitacaedmy.org>
2024-11-14 11:24:44 +0100

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  watchForFileChanges:false,
+  failOnStatusCode: false,
+  e2e: {
+    reporter: 'cypress-mochawesome-reporter',
+    baseUrl: 'https://gorest.co.in',
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      on('task', {
+        log(message) {
+            console.log(message + '\n');
+            return null;
+        },
+    
+      });
+    },
+  
+  env:{
+   
+    ACCESS_TOKEN:"1e7ced5d72468d2b5f8263be474161e3e8f076758695e340ae8f597055773b0b"
+  }
+},
+});
+
